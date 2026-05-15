a0e4170853b26261f06b494bd07f64125b919f71
Rasel66 <raselhossain5437@gmail.com>
2026-05-15 12:32:52 +0600

    static file added

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -10,7 +10,5 @@ urlpatterns = [
     path('permissions/', include('permission_app.urls')),
     path('', include('client_app.urls')),
     path('report/', include('report_app.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
