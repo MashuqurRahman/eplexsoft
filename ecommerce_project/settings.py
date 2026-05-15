3d2971b78e765a2fb1c1ecdde992d0b01808aa4a
Rasel66 <raselhossain5437@gmail.com>
2026-05-15 11:41:25 +0600

    duplicate entry solve

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -103,9 +103,9 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.mysql',
 
-        'NAME': os.environ.get('DJANGO_DB_NAME', 'eplexsoft_db'),
-        'USER': os.environ.get('DJANGO_DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', ''),
+        'NAME': os.environ.get('DJANGO_DB_NAME', 'alianceb_ecommerce'),
+        'USER': os.environ.get('DJANGO_DB_USER', 'alianceb_db_admin'),
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', 'aliance@brothers'),
         'HOST': os.environ.get('DJANGO_DB_HOST','127.0.0.1'),
 
         'PORT': os.environ.get('PORT', '3306'),
@@ -157,7 +157,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
